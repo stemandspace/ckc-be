afeba50ac963326a58c44f35dfa14dea6eef98ee
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-01-23 14:49:52 +0530

    attempt added in get daily spin

--- src/api/daily-spin/controllers/get-daily-spin.js
+++ src/api/daily-spin/controllers/get-daily-spin.js
@@ -3,6 +3,10 @@
 const controller = ({ strapi }) => ({
   async getDailySpin(ctx) {
     try {
+      const { userId } = ctx.request.query;
+      if (!userId) {
+        return ctx.send("Please provide user id", 400);
+      }
       const dailySpin = await strapi.db
         .query("api::daily-spin.daily-spin")
         .findOne({
@@ -10,7 +14,20 @@ const controller = ({ strapi }) => ({
             slices: true,
           },
         });
-      return ctx.send({ dailySpin }, 200);
+      const startOfDay = new Date();
+      startOfDay.setUTCHours(0, 0, 0, 0);
+      const endOfDay = new Date();
+      endOfDay.setUTCHours(23, 59, 59, 999);
+      const attempt = await strapi.db.query("api::spin.spin").findOne({
+        where: {
+          user_id: userId || 0,
+          publishedAt: {
+            $gte: startOfDay.toISOString(),
+            $lt: endOfDay.toISOString(),
+          },
+        },
+      });
+      return ctx.send({ dailySpin, attempt: attempt ? false : true }, 200);
     } catch (err) {
       console.error(err);
       ctx.internalServerError("An error occurred during get daily spin.");
