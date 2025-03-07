e28b7fa3694ebfe26739ce1da7b49d0966c09f31
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-03-07 12:26:31 +0530

    Remove .env file and update .gitignore to exclude it; refactor AWS configuration in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -136,7 +136,6 @@ module.exports = ({ env }) => ({
           secretAccessKey: env("AWS_ACCESS_SECRET"),
           region: env("AWS_REGION"),
           params: {
-            // ACL: env("AWS_ACL", "public-read"),
             signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
             Bucket: env("AWS_BUCKET"),
           },
@@ -153,9 +152,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "amazon-ses",
       providerOptions: {
-        key: env("AWS_SES_ACCESS_KEY_ID"),
-        secret: env("AWS_SES_ACCESS_SECRET"),
-        amazon: "https://email.us-east-1.amazonaws.com",
+        region: env("AWS_REGION"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
       },
       settings: {
         defaultFrom: "hello@cosmickids.club",
