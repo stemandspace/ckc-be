6c7307d33c1192e5fdbd5652b46298f6ceef2837
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-03-25 16:04:43 +0530

    Add new route for fetching achievements: implement GET /get-achievements handler in the custom API routes.

---
+++ src/api/achivement/controllers/get-achivements.js
@@ -0,0 +1,97 @@
+/**
+ * @typedef {import('@strapi/strapi').Strapi} Strapi
+ * @typedef {import('koa').Context} Context
+ */
+
+/**
+ * NAC custom APIs
+ * @param {{ strapi: Strapi }} param0 - Strapi instance.
+ * @returns {Object} Controller methods.
+ */
+// Time Tracking Reward
+const controller = ({ strapi }) => ({
+  async getAchievements(ctx) {
+    try {
+      const { userId } = ctx.request.query;
+      const achievements = await strapi.db
+        .query("api::achivement.achivement")
+        .findMany({
+          where: {
+            user: Number(userId),
+            contentType: {
+              $in: [
+                "quiz",
+                "banner",
+                "avatar",
+                "certificate",
+                "badge",
+                "coins",
+                "bannar",
+                "referral",
+                "credit",
+                "marketplacePromocode",
+                "systemPromocode",
+              ],
+            },
+          },
+          orderBy: { createdAt: "desc" },
+        });
+      const populateAchievementsReward = await Promise.all(
+        achievements.map(async (achivement) => {
+          if (achivement?.rewardId) {
+            const reward = await strapi.db.query("api::reward.reward").findOne({
+              where: {
+                id: Number(achivement.rewardId),
+              },
+              populate: {
+                marketplace_promocode: {
+                  populate: {
+                    shopify_price_rule: true,
+                  },
+                },
+                system_promocode: true,
+                badge: true,
+                avatar: true,
+                certificate: true,
+                bannar: true,
+              },
+            });
+            return { ...achivement, reward };
+          }
+          return achivement;
+        })
+      );
+      const categorizedAchievements = {
+        quiz: [],
+        banner: [],
+        avatar: [],
+        certificate: [],
+        badge: [],
+        coins: [],
+        bannar: [],
+        referral: [],
+        credit: [],
+        marketplacePromocode: [],
+        systemPromocode: [],
+      };
+
+      // Populate the categorized achievements
+      populateAchievementsReward.forEach((achievement) => {
+        const type = achievement.contentType;
+        if (categorizedAchievements[type]) {
+          categorizedAchievements[type].push(achievement);
+        }
+      });
+      return ctx.send(
+        {
+          categorizedAchievements,
+        },
+        200
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
+
+module.exports = controller;
--- src/api/achivement/routes/custom.js
+++ src/api/achivement/routes/custom.js
@@ -5,5 +5,10 @@ module.exports = {
       path: "/leaderboard", // Only match when the URL parameter is composed of lowercase letters
       handler: "achivement.leaderboard",
     },
+    {
+      method: "GET",
+      path: "/get-achievements",
+      handler: "get-achivements.getAchievements",
+    },
   ],
 };
